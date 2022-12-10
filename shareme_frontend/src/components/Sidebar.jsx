9b95db8157532dad8dfd49a8fed22cbf02d731d3
naresh-webdev <gkmnaresh01@gmail.com>
2022-12-10 12:59:10 +0530

    start of 10/12/22

--- shareme_frontend/src/components/Sidebar.jsx
+++ shareme_frontend/src/components/Sidebar.jsx
@@ -6,7 +6,24 @@ import { IoIosArrowForward } from "react-icons/io";
 import logo from "../assets/logo.png";
 
 const Sidebar = ({ user, closeToggle }) => {
-  return <div>Sidebar</div>;
+  const hanadleCloseSidebar = () => {
+    if (closeToggle) closeToggle(false);
+  };
+
+  return (
+    <div className="flex flex-col justify-between bg-white h-full overflow-y-scroll min-w-210 hide-scrollbar">
+      <div className="flex flex-col">
+        <Link
+          to="/"
+          className="flex px-5 gap-2 my-6 pt-1 w-190 items-center"
+          onClick={hanadleCloseSidebar}
+        >
+          <img src={logo} alt="logo img" className="w-full" />
+        </Link>
+      </div>
+      Sidebar
+    </div>
+  );
 };
 
 export default Sidebar;
